3228bc87b81fc7daf4be2898a11f02b3b23b4dc2
Duyet Le <5009534+duyet@users.noreply.github.com>
2021-12-13 22:11:55 +0700

    chore(grant): update sha256

--- Formula/grant.rb
+++ Formula/grant.rb
@@ -2,7 +2,7 @@ class Grant < Formula
   desc "Manage Redshift/Postgres privileges in GitOps style written in Rust"
   homepage "https://github.com/duyet/grant.rs"
   url "https://github.com/duyet/grant.rs/releases/download/0.0.1-beta.3/grant-0.0.1-beta.3-x86_64-apple-darwin.tar.gz"
-  sha256 "55bce4932a1b68522944aac875c0b31ebfd95d53"
+  sha256 "2428ccaa85272bfd55777675d30a4248828667b226bde26c964eaf5776ba7cb1"
   version "0.0.1-beta.3"
 
   def install
