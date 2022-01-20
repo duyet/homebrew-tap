a42c42182020951f329afb78f38632e8b3237691
Duyet Le <5009534+duyet@users.noreply.github.com>
2022-01-20 11:53:47 +0700

    Update grant.rb

--- Formula/grant.rb
+++ Formula/grant.rb
@@ -2,7 +2,7 @@ class Grant < Formula
   desc "Manage Redshift/Postgres privileges in GitOps style written in Rust"
   homepage "https://github.com/duyet/grant.rs"
   url "https://github.com/duyet/grant.rs/releases/download/0.0.1-beta.3/grant-0.0.1-beta.3-x86_64-apple-darwin.tar.gz"
-  sha256 "2428ccaa85272bfd55777675d30a4248828667b226bde26c964eaf5776ba7cb1"
+  sha256 "43575bdd4399a77abb7442a1ace780eb42f258862f5cb9111c8d7b0652b5c47f"
   version "0.0.1-beta.3"
 
   def install
